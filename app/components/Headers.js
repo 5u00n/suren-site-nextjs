e7ec22493be7651a82364ab46eb9233ab84960c4
5u00n <surenhembram97@gmail.com>
2024-09-24 01:09:37 -0700

    Refactor Headers.js for responsive design improvements and font loading/styling enhancements

--- app/components/Headers.js
+++ app/components/Headers.js
@@ -10,33 +10,35 @@ const unica_one = Unica_One({
 });
 function Headers() {
   return (
-    <header className="md:mt-10 sm:px-5 lg:px-16 w-screen  z-50 ">
-      <nav className="flex items-center justify-between mx-auto  ">
-        <Link href="/" className={`${unica_one.className} text-[38px] font-bold `}>
-          SH
-        </Link>
-        <ul className="flex space-x-[40px] items-center font-figtree">
-          <li>
-            <Link href="/">
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link href="/services">
-              Services
-            </Link>
-          </li>
-          <li>
-            <Link href="/projects">
-              Projects
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              Blog
-            </Link>
-          </li>
-          <li>
+    <nav className="flex items-center justify-between mx-auto w-screen md:mt-5 xs:px-5 lg:px-16 z-50 nav  ">
+      <Link href="/" className={`${unica_one.className} text-[38px] font-bold `}>
+        SH
+      </Link>
+      <ul className="flex space-x-[40px] items-center font-figtree">
+        <li>
+          <Link href="/">
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link href="/services">
+            Services
+          </Link>
+        </li>
+        <li>
+          <Link href="/projects">
+            Projects
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            Blog
+          </Link>
+        </li>
+      </ul>
+
+      <ul className="flex space-x-5 items-center">
+      <li>
             <Link href="/hire-me">
               <button className="bg-[#458a4e] hover:bg-[#2d6e35] text-white font-bold py-2 px-4 rounded">
                 Hire Me
@@ -51,8 +53,7 @@ function Headers() {
             </Link>
           </li>
         </ul>
-      </nav>
-    </header>
+    </nav>
   )
 }
 
