3e0888ec1307828a37724667146655a1897b0bf4
5u00n <surenhembram97@gmail.com>
2024-10-08 23:45:30 +0530

    Refactor page.js to update margin-top value in works and awards section

--- app/page.js
+++ app/page.js
@@ -105,7 +105,7 @@ export default function Home() {
         </div>
       </hero>
 
-      <section id="projects-section" className=" flex flex-col gap-8 md:px-10 sm:px-5 xs:px-10 bg-[#ECF2FF] dark:bg-[#314061] py-3  max-w-[1280px] mx-auto xs:w-screen">
+      <section id="projects-section" className=" flex flex-col gap-8 md:px-10 sm:px-5 xs:px-10 bg-[#ECF2FF] dark:bg-[#314061] py-3  max-w-[1280px] mx-auto xs:w-full">
         <ul className="h-full grid grid-cols-2 gap-4 mx-auto w-full md:w-full md:grid-cols-4 ">
           <li className="w-full h-full flex flex-col ">
             <h2 className=" text-heading3 font-bold w-full font-figtree">
